b5a5662f022957045b425dc2fc12bdc48c1b9134
oliver3428402 <olivermolenschot@gmail.com>
2021-10-13 20:12:46 +0200

    branching out Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-
+#This file should be in the second branch of my directory
 class Cards:
     cardnumber = [2,3,4,5,6,7,8,9,10,'J','Q','K','A',2,3,4,5,6,7,8,9,10,'J','Q','K','A',2,3,4,5,6,7,8,9,10,'J','Q','K','A',2,3,4,5,6,7,8,9,10,'J','Q','K','A']
     cardsuit = ['Spades','Hearts','Clubs','Diamonds']
